5b549789bd6f4ad8296a1753b41e86339f716063
Zhen Wang <mail@zhenwang.info>
2015-03-23 12:37:59 +0800

    Support function level (number) for setfenv/getfenv compat functions

--- core/env/fenv.lua
+++ core/env/fenv.lua
@@ -1,22 +1,30 @@
 local cache = setmetatable({}, {__mode = 'k'})
 
+
 local function find_env(f)
   local up = 0
   local name, val
 
-  f = (type(f) == 'function' and f or debug.getinfo(f + 1, 'f').func)
+  if type(f) == 'number' then
+    assert(f >= 0, 'Function level must be positive')
+    assert(f ~= 0, 'Thread environments not supported')
+    f = debug.getinfo(f + 2, 'f').func
+  end
+  assert(type(f) == 'function', '[function] or [number] expected')
+  assert(debug.getinfo(f).what ~= 'C', 'C function not supported')
+
   repeat
     up = up + 1
     name, val = debug.getupvalue(f, up)
   until name == '_ENV' or name == nil
 
-  return up, val
+  return up, val, f
 end
 
 
 setfenv = rawget(_G, 'setfenv') or function(f, t)
   assert(f and t)
-  local up, val = find_env(f)
+  local up, val, f = find_env(f)
   if val then
     debug.upvaluejoin(f, up, function() return t end, 1)
   else
@@ -28,7 +36,7 @@ end
 
 getfenv = rawget(_G, 'getfenv') or function(f)
   assert(f)
-  local val = select(2, find_env(f))
+  local _, val, f = find_env(f)
   if val then return val end
 
   if not cache[f] then cache[f] = {} end
