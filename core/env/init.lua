f1b2a9ffb039689788dea9e951cbad3913bf918c
Zhen Wang <mail@zhenwang.info>
2015-04-18 10:46:18 +0800

    Added table compat and helpers module

--- core/env/init.lua
+++ core/env/init.lua
@@ -1,2 +1,4 @@
 require('core.env.strict')
 require('core.env.fenv')
+require('core.env.table')
+
---
+++ core/env/table.lua
@@ -0,0 +1,56 @@
+function table.keys(t)
+  local keys = {}
+  for k in pairs(t) do
+    table.insert(keys, k)
+  end
+  return keys
+end
+
+
+function table.len(t)
+  return #table.keys(t)
+end
+
+
+function table.print(t)
+  local Observable = require('core.Observable')
+
+  local function tprint(v, indent, visited)
+    if not v then
+      print('nil')
+      return
+    end
+
+    if not visited then
+      print(tostring(v))
+    end
+
+    visited = visited or {}
+    indent = (indent or 0) + 2
+
+    if type(v) == 'table' then
+      local iter = Observable.is_observable(v) and Observable.spairs or pairs
+      for key, value in iter(v) do
+        local fmt = '%' .. indent .. 's[%s] => %s'
+        print(string.format(fmt, '', tostring(key), tostring(value)))
+
+        if Observable.is_indexable(value) and not visited[value] then
+          visited[value] = true
+          tprint(value, indent, visited)
+        end
+      end
+    end
+  end
+
+  return tprint(t)
+end
+
+
+function table.vivify(t, keys, n)
+  for i = 1, n or #keys do
+    local v = keys[i]
+    if t[v] == nil then t[v] = {} end
+    t = t[v]
+  end
+  return t
+end
