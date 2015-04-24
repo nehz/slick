bb1e2eac8d8dc4b8a7f412307dba24a171dc04ca
Zhen Wang <mail@zhenwang.info>
2015-04-24 20:48:31 +0800

    Update Dispatcher. Require `key` to access and remove items. No longer allowed to access items that no longer exist.

--- core/Dispatcher.lua
+++ core/Dispatcher.lua
@@ -4,7 +4,13 @@ local Dispatcher = {}
 function Dispatcher.new(max_items)
   max_items = tonumber(max_items)
   assert(max_items, 'Expected number for max items')
-  return setmetatable({next = 0, n = 0, max = max_items}, Dispatcher)
+  local dispatcher = {
+    next = 0,
+    n = 0,
+    max = max_items,
+    cycle = 0,
+  }
+  return setmetatable(dispatcher, Dispatcher)
 end
 
 
@@ -17,19 +23,38 @@ function Dispatcher.assign(d, obj)
     id = d.next
     d.next = (id + 1) % d.max
   until d[id] == nil
-  d[id] = obj
+  if id == 0 then d.cycle = d.cycle + 1 end
+  d[id] = {obj, d.cycle}
 
-  return id
+  return id, d.cycle
 end
 
 
-function Dispatcher.remove(d, id)
+function Dispatcher.check(d, id, key)
+  assert(d[id], 'No item at id ' .. id)
+  assert(d[id][2] == key, 'Invalid key for item at id ' .. id)
+end
+
+
+function Dispatcher.remove(d, id, key)
+  Dispatcher.check(d, id, key)
   if d[id] == nil then return end
   d.n = d.n - 1
 
   local res = d[id]
   d[id] = nil
-  return res
+  return res[1]
+end
+
+
+function Dispatcher.get(d, id, key)
+  Dispatcher.check(d, id, key)
+  return table.unpack(d[id])
+end
+
+
+function Dispatcher:__tostring()
+  return string.format('Dispatcher(n: %d, max: %d)', self.n, self.max)
 end
 
 
--- test/core/Dispatcher_spec.lua
+++ test/core/Dispatcher_spec.lua
@@ -10,72 +10,99 @@ describe('Dispatcher', function()
 
   it('should assign item', function()
     local d = Dispatcher.new(10)
-    local a = Dispatcher.assign(d, 1)
-    local b = Dispatcher.assign(d, 2)
+    local a, a_key = Dispatcher.assign(d, 1)
+    local b, b_key = Dispatcher.assign(d, 2)
 
-    assert.is.equal(d[a], 1)
-    assert.is.equal(d[b], 2)
+    assert.is.equal(Dispatcher.get(d, a, a_key), 1)
+    assert.is.equal(Dispatcher.get(d, b, b_key), 2)
   end)
 
   it('should remove item', function()
     local d = Dispatcher.new(10)
-    local a = Dispatcher.assign(d, 1)
-    local b = Dispatcher.assign(d, 2)
+    local a, a_key = Dispatcher.assign(d, 1)
+    local b, b_key = Dispatcher.assign(d, 2)
 
-    assert.is.equal(d[a], 1)
-    assert.is.equal(d[b], 2)
+    assert.is.equal(Dispatcher.get(d, a, a_key), 1)
+    assert.is.equal(Dispatcher.get(d, b, b_key), 2)
     assert.is.equal(d.n, 2)
 
-    assert.is.equal(Dispatcher.remove(d, a), 1)
-    assert.is.equal(Dispatcher.remove(d, b), 2)
+    assert.is.equal(Dispatcher.remove(d, a, a_key), 1)
+    assert.is.equal(Dispatcher.remove(d, b, b_key), 2)
     assert.is.equal(d.n, 0)
     assert.is.equal(d[a], nil)
     assert.is.equal(d[b], nil)
-    assert.is.same(d, {max = 10, next = d.next, n = 0})
   end)
 
   it('should be reassignable', function()
     local d = Dispatcher.new(2)
-    local a = Dispatcher.assign(d, 1)
-    local b = Dispatcher.assign(d, 2)
+    local a, a_key = Dispatcher.assign(d, 1)
+    local b, b_key = Dispatcher.assign(d, 2)
 
-    assert.is.equal(d[a], 1)
-    assert.is.equal(d[b], 2)
+    assert.is.equal(Dispatcher.get(d, a, a_key), 1)
+    assert.is.equal(Dispatcher.get(d, b, b_key), 2)
 
-    assert.is.equal(Dispatcher.remove(d, a), 1)
-    assert.is.equal(Dispatcher.remove(d, b), 2)
+    assert.is.equal(Dispatcher.remove(d, a, a_key), 1)
+    assert.is.equal(Dispatcher.remove(d, b, b_key), 2)
     assert.is.equal(d[a], nil)
     assert.is.equal(d[b], nil)
 
-    a = Dispatcher.assign(d, 3)
-    b = Dispatcher.assign(d, 4)
-    assert.is.equal(d[a], 3)
-    assert.is.equal(d[b], 4)
+    a, a_key = Dispatcher.assign(d, 3)
+    b, b_key = Dispatcher.assign(d, 4)
+    assert.is.equal(Dispatcher.get(d, a, a_key), 3)
+    assert.is.equal(Dispatcher.get(d, b, b_key), 4)
     assert.is.equal(d.n, d.max, 2)
-    assert.is.same(d, {[a] = 3, [b] = 4, max = 2, next = d.next, n = 2})
   end)
 
   it('should not assign item if full', function()
     local d = Dispatcher.new(2)
-    local a = Dispatcher.assign(d, 1)
-    local b = Dispatcher.assign(d, 2)
+    local a, a_key = Dispatcher.assign(d, 1)
+    local b, b_key = Dispatcher.assign(d, 2)
 
     assert.has_error(function() Dispatcher.assign(d, 3) end,
       'Unable to allocate')
 
-    assert.is.equal(d[a], 1)
-    assert.is.equal(d[b], 2)
-    assert.is.same(d, {[a] = 1, [b] = 2, max = 2, next = d.next, n = 2})
+    assert.is.equal(Dispatcher.get(d, a, a_key), 1)
+    assert.is.equal(Dispatcher.get(d, b, b_key), 2)
 
-    assert.is.equal(Dispatcher.remove(d, b), 2)
-    assert.is.equal(d[a], 1)
-    assert.is.equal(d[b], nil)
+    assert.is.equal(Dispatcher.remove(d, b, b_key), 2)
+    assert.is.equal(Dispatcher.get(d, a, a_key), 1)
     assert.is.equal(d.n, 1)
 
     b = Dispatcher.assign(d, 3)
-    assert.is.equal(d[a], 1)
-    assert.is.equal(d[b], 3)
+    assert.is.equal(Dispatcher.get(d, a, a_key), 1)
+    assert.is.equal(Dispatcher.get(d, b, b_key), 3)
     assert.is.equal(d.n, 2)
-    assert.is.same(d, {[a] = 1, [b] = 3, max = 2, next = d.next, n = 2})
+  end)
+
+  it('should prevent access to items that no longer exist', function()
+    local d = Dispatcher.new(2)
+    local a, a_key = Dispatcher.assign(d, 1)
+    assert.is.equal(Dispatcher.remove(d, a, a_key), 1)
+    assert.is.equal(d.n, 0)
+
+    assert.has_error(function() Dispatcher.check(d, a, a_key) end,
+      'No item')
+    assert.has_error(function() Dispatcher.get(d, a, a_key) end,
+      'No item')
+    assert.has_error(function() Dispatcher.remove(d, a, a_key) end,
+      'No item')
+  end)
+
+  it('should require key', function()
+    local d = Dispatcher.new(2)
+    local a, a_key = Dispatcher.assign(d, 1)
+
+    assert.has_error(function() Dispatcher.check(d, a, a_key + 1) end,
+      'Invalid key')
+    Dispatcher.check(d, a, a_key)
+
+    assert.has_error(function() Dispatcher.get(d, a, a_key + 1) end,
+      'Invalid key')
+    assert.is.equal(Dispatcher.get(d, a, a_key), 1)
+
+    assert.has_error(function() Dispatcher.remove(d, a, a_key + 1) end,
+      'Invalid key')
+    assert.is.equal(Dispatcher.remove(d, a, a_key), 1)
+    assert.is.equal(d.n, 0)
   end)
 end)
