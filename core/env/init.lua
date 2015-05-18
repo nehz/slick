6dd1030371c2a7e8b9c69b29042ea72988e9b2ff
Zhen Wang <mail@zhenwang.info>
2015-05-18 18:06:19 +0800

    Patch assert(). Allow multiple parameters.

--- core/env/init.lua
+++ core/env/init.lua
@@ -2,3 +2,12 @@ require('core.env.strict')
 require('core.env.fenv')
 require('core.env.table')
 
+
+_assert = assert
+function assert(v, ...)
+  if not v then
+    local args = table.pack(...)
+    error(table.concat(args, ' ', 1, args.n), 2)
+  end
+  return v
+end
